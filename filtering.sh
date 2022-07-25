74b5ca90918ad2cf5a64bae1d1f16928a3a1be20
DandiMahendris <dandi.mahendris@gmail.com>
2022-07-25 08:05:36 +0700

    add picture and new filtering.sh

--- filtering.sh
+++ filtering.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cat 2019-sample-clean.csv | grep electronics | grep smartphone | awk -F ', '{print $5} | sort | uniq -c | sort -nr
+cat 2019-sample-clean.csv | wc
+
+cat 2019-sample-clean.csv | grep electronics | grep smartphone | awk -F ',' '{print $5}' | sort | uniq -c | sort -nr
